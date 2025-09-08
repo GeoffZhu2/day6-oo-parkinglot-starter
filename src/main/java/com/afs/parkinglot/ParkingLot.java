767931f2560962b891497e4053bf68df36a91116
zhuhao <zhuhao4396@foxmail.com>
2025-09-08 19:22:55 +0800

    feat:return a ticket given a parking lot and a car when parking car

--- src/main/java/com/afs/parkinglot/ParkingLot.java
+++ src/main/java/com/afs/parkinglot/ParkingLot.java
@@ -1,4 +1,22 @@
 package com.afs.parkinglot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
+    private int capacity;
+    private final Map<Ticket, Car> ticketCarMap = new HashMap<>();
+    public ParkingLot(int capacity) {
+
+        this.capacity = capacity;
+    }
+    public Ticket park(Car car) {
+        if(capacity > 0) {
+            capacity--;
+            Ticket ticket = new Ticket();
+            ticketCarMap.put(ticket, car);
+            return ticket;
+        }
+        return null;
+    }
 }
--- src/test/java/com/afs/parkinglot/ParkingLotTest.java
+++ src/test/java/com/afs/parkinglot/ParkingLotTest.java
@@ -1,4 +1,13 @@
 package com.afs.parkinglot;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ParkingLotTest {
+    @Test
+    public void should_return_a_ticket_given_a_parking_lot_and_a_car_when_parking_car() {
+        ParkingLot parkingLot = new ParkingLot(10);
+        assertNotNull(parkingLot.park(new Car()));
+    }
 }
