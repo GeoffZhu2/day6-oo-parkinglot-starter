bbd3db0251f23fa13244d119cf9acecfc5d12b6b
Geoff Zhu <zhuge2@oocl.com>
2025-09-09 10:05:44 +0800

    refactor:move sort from construction to park

--- src/main/java/com/afs/parkinglot/SmartParkingBoy.java
+++ src/main/java/com/afs/parkinglot/SmartParkingBoy.java
@@ -5,6 +5,11 @@ import java.util.List;
 public class SmartParkingBoy extends StandardParkingBoy {
     public SmartParkingBoy(List<ParkingLot> parkingLots) {
         super(parkingLots);
+
+    }
+    @Override
+    public Ticket park(Car car) {
         this.parkingLots.sort((p1, p2) -> Integer.compare(p2.getCapacity(), p1.getCapacity()));
+        return super.park(car);
     }
 }
--- src/main/java/com/afs/parkinglot/SuperParkingBoy.java
+++ src/main/java/com/afs/parkinglot/SuperParkingBoy.java
@@ -6,11 +6,15 @@ public class SuperParkingBoy extends StandardParkingBoy {
 
     public SuperParkingBoy(List<ParkingLot> parkingLots) {
         super(parkingLots);
+    }
+    @Override
+    public Ticket park(Car car) {
         this.parkingLots.sort((p1, p2) ->
                 Double.compare(
                         (double) p2.getAvailablePosition() / p2.getCapacity(),
                         (double) p1.getAvailablePosition() / p1.getCapacity()
                 ));
+        return super.park(car);
     }
 
 }
